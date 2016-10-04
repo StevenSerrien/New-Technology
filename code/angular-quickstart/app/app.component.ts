f463e11a4d9420e83f1e32c2eee13c11d0da984b
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-04 16:48:02 +0200

    5 Minute Quickstart Angular 2
    Source:
    https://angular.io/docs/ts/latest/quickstart.html#!#create-the-project-folder

--- code/angular-quickstart/app/app.component.ts
+++ code/angular-quickstart/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My FOURTH Angular App</h1>'
+  template: '<h1>My FIFTH Angular App</h1>'
 })
 export class AppComponent { }
