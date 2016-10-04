fcb568ad16136f420d2ee9957163b5a56c34769f
StevenSerrien <steven.serrien@student.kdg.be>
2016-10-04 18:18:24 +0200

    Chapter  1 - Our First Component

---
+++ code/Angular 2 - Accelerating through Angular 2/racing-app/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'racing-app',
+  template: '<h1>{{heading}}</h1>'
+})
+export class AppComponent {
+  heading = "Ultra Racing Schedule"
+ }
